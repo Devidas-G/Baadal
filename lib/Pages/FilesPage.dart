0827fc47cbab38e25d002a52f869f14629cebee3
Devidas-G <dev.gaikwad222@gmail.com>
2021-07-16 13:32:19 +0530

    commit

--- lib/Pages/FilesPage.dart
+++ lib/Pages/FilesPage.dart
@@ -1,4 +1,8 @@
+import 'package:baadal/Global%20variables.dart';
+import 'package:baadal/Services/DatabaseServices.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+
 class FilesPage extends StatefulWidget {
   const FilesPage({Key? key}) : super(key: key);
 
@@ -9,8 +13,80 @@ class FilesPage extends StatefulWidget {
 class _FilesPageState extends State<FilesPage> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.white,
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Container(
+              //AppBar
+              height: 55,
+              width: double.infinity,
+              decoration: BoxDecoration(color: Colors.white, boxShadow: [
+                BoxShadow(
+                    color: Colors.grey, offset: Offset(0, 3), blurRadius: 6)
+              ]),
+              child: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
+                              child: Text(
+                                "root/",
+                                style: TextStyle(color: myColor, fontSize: 24),
+                              )),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(left: 10.0),
+                          child: Text("Directory"),
+                        ),
+                      ],
+                    ),
+                  ),
+                  IconButton(
+                      onPressed: () {
+                        //TODO: copy button onTap
+                      },
+                      icon: Icon(
+                        Icons.copy,
+                        color: myColor,
+                      ))
+                ],
+              ),
+            ),
+            Expanded(
+                child: StreamBuilder(
+                stream: DatabaseServices().userFiles.doc(user!.uid).collection('userFiles').orderBy('time').snapshots(),
+                builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (!snapshot.hasData) {
+                  return Center(child: Text('Loading'));
+                } else if (snapshot.data!.docs.isEmpty) {
+                  //print(snapshot.data.documents);
+                  return Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Image.asset("images/BlueCloud.png",height: 150,),
+                      Text("No file is your cloud drive"),
+                    ],
+                  );
+                }
+                return ListView(
+                    physics: BouncingScrollPhysics(),
+                    children: snapshot.data!.docs
+                        .map((doc) => ListTile(
+                              title: Row(),
+                            ))
+                        .toList());
+              },
+            )),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/Pages/ProfilePage.dart
+++ lib/Pages/ProfilePage.dart
@@ -1,5 +1,8 @@
 import 'package:baadal/Global%20variables.dart';
+import 'package:baadal/Services/AuthServices.dart';
 import 'package:baadal/Services/DatabaseServices.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -12,58 +15,140 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    var size =MediaQuery.of(context).size;
+    var size = MediaQuery.of(context).size;
     return Scaffold(
         body: SafeArea(
       child: Column(
         children: [
           FutureBuilder(
               future: DatabaseServices().getUserInfo(),
-              builder: (BuildContext context,
-              AsyncSnapshot<dynamic> snapshot) {
-            return Row(
+              builder: (BuildContext context, AsyncSnapshot<dynamic> snapshot) {
+                return Container(
+                  height: 80,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    color: Colors.white,
+                    boxShadow: [BoxShadow(color: Colors.grey,offset: Offset(0,3),blurRadius: 6)],
+                  ),
+                  child: Row(
+                    children: [
+                      Row(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Padding(
+                            padding: const EdgeInsets.all(8.0),
+                            child: Container(
+                              height: 70,
+                              width:70,
+                              decoration: BoxDecoration(
+                                //color: Colors.green,
+                                  borderRadius:
+                                  BorderRadius.all(Radius.circular(90))),
+                              child: (() {
+                                switch (snapshot.connectionState) {
+                                  case ConnectionState.none:
+                                    return Image.asset("images/Default-Profile.png");
+                                  case ConnectionState.waiting:
+                                    return Image.asset("images/Default-Profile.png");
+                                  case ConnectionState.active:
+                                    return Image.asset("images/Default-Profile.png");
+                                  case ConnectionState.done:
+                                    Map<String, dynamic> data = snapshot.data;
+                                    if ({data['profileUrl']}.isEmpty)
+                                      return Image.asset(
+                                          "images/Default-Profile.png");
+                                    if (snapshot.hasError)
+                                      return Image.asset(
+                                          "images/Default-Profile.png");
+                                    if (data['profileUrl'] == null)
+                                      return Image.asset(
+                                          "images/Default-Profile.png");
+                                    if ({data['profileUrl']}.isNotEmpty)
+                                      return ClipRRect(
+                                          borderRadius: BorderRadius.circular(90),
+                                          child: Image.network(
+                                            '${data['profileUrl']}',
+                                            fit: BoxFit.cover,
+                                          ));
+                                }
+                                return Image.asset("images/Default-Profile.png");
+                              }()),
+                            ),
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 20.0, left: 10),
+                            child: (() {
+                              switch (snapshot.connectionState) {
+                                case ConnectionState.none:
+                                  return Text("Loading UserName...");
+                                case ConnectionState.waiting:
+                                  return Text("Loading UserName...");
+                                case ConnectionState.active:
+                                  return Text("Loading UserName...");
+                                case ConnectionState.done:
+                                  Map<String, dynamic> data = snapshot.data;
+                                  if ({data['name']}.isEmpty) {
+                                    return Text(
+                                      "Enter name",
+                                      style: TextStyle(fontSize: 18,color: myColor),
+                                    );
+                                  } else if (snapshot.hasError) {
+                                    return Text(
+                                      "Loading UserName...",
+                                      style: TextStyle(fontSize: 18,color: myColor),
+                                    );
+                                  } else if (data['name'] == null) {
+                                    return Text(
+                                      "Enter name",
+                                      style: TextStyle(fontSize: 18,color: myColor),
+                                    );
+                                  }else if (data['name'] == "") {
+                                    return Text(
+                                      "Enter name",
+                                      style: TextStyle(fontSize: 18,color: myColor),
+                                    );
+                                  } else if ({data['name']}.isNotEmpty) {
+                                    return Text(
+                                      "${data['name']}",
+                                      style: TextStyle(fontSize: 18,color: myColor),
+                                    );
+                                  }
+                              }
+                              Text(
+                                "Loading UserName...",
+                              );
+                            }()),
+                          ),
+                        ],
+                      ),
+                      Expanded(
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            IconButton(onPressed: (){
+                              //TODO: Navigate to edit profile
+                            }, icon: Icon(Icons.arrow_forward_ios,color: myColor,))
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+          InkWell(
+            onTap: (){
+              AuthService(FirebaseAuth.instance).logout();
+            },
+            child: Row(
               children: [
                 Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Container(
-                    height: size.height * 0.1,
-                    width: size.width * 0.2,
-                    decoration: BoxDecoration(
-                      //color: Colors.green,
-                        borderRadius:
-                        BorderRadius.all(Radius.circular(90))),
-                    child: ((){
-                      switch(snapshot.connectionState){
-                        case ConnectionState.none:
-                          return Image.asset("images/Default-Profile.png");
-                        case ConnectionState.waiting:
-                          return Image.asset("images/Default-Profile.png");
-                        case ConnectionState.active:
-                          return Image.asset("images/Default-Profile.png");
-                        case ConnectionState.done:
-                          Map<String, dynamic> data = snapshot.data;
-                          if({data['profileUrl']}.isEmpty)
-                            return Image.asset("images/Default-Profile.png");
-                          if(snapshot.hasError)
-                            return Image.asset("images/Default-Profile.png");
-                          if(data['profileUrl']==null)
-                            return Image.asset("images/Default-Profile.png");
-                          if({data['profileUrl']}.isNotEmpty)
-                            return ClipRRect(
-                                borderRadius: BorderRadius.circular(90),
-                                child: Image.network(
-                                  '${data['profileUrl']}',
-                                  fit: BoxFit.cover,
-                                ));
-
-                      }
-                      return Image.asset("images/Default-Profile.png");
-                    }()),
-                  ),
+                  padding: const EdgeInsets.all(18.0),
+                  child: Icon(Icons.logout,color: myColor,size: 30,),
                 ),
+                Text("Logout",style: TextStyle(color: myColor,fontSize: 18),),
               ],
-            );
-          }),
+            ),
+          ),
         ],
       ),
     ));
